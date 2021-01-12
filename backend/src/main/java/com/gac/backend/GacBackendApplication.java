da61fc5be96704010602aa45337887e4b9c44161
Antoine Malhache <t0203866@LILLE.TIS>
2021-01-12 15:57:32 +0100

    initialization of the project

---
+++ backend/src/main/java/com/gac/backend/GacBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gac.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GacBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GacBackendApplication.class, args);
+	}
+
+}
