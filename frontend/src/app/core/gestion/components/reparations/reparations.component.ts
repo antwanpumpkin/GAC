36522055a7364196b538e114dc712dfd533b1a0a
Antoine Malhache <antoine.malhache@gmail.com>
2021-09-06 11:45:10 +0200

    chore(reparation): ajout reparation list et contenu de la page

--- frontend/src/app/core/gestion/components/reparations/reparations.component.ts
+++ frontend/src/app/core/gestion/components/reparations/reparations.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reparations.component.css']
 })
 export class ReparationsComponent implements OnInit {
+  secteurList: any;
+  reparationList: any;
 
-  constructor() { }
+  
+  secteurSelected = null;  
+  reparationSelected = null;
+  private _jsonURL = 'assets/reparations.json';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get(this._jsonURL).subscribe(data =>{
+      this.secteurList = data;
+    })
+  }
+
+  selectionSecteur(event: any) {
+    this.secteurSelected = event.value
+    this.reparationList = this.secteurList.filter(list => list.secteur == event.value)[0];
+  }
+
+  selectionReparation(event: any) {
+    this.reparationSelected = event.value;
   }
 
 }
