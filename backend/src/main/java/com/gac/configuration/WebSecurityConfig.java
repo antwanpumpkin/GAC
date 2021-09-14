79660e1d6acbe1cab4b405d2a017d67710e7d290
Antoine MALHACHE <antoine.malhache@gmail.com>
2021-09-14 15:10:56 +0200

    chore(jwt): recupération user depuis bdd

--- backend/src/main/java/com/gac/configuration/WebSecurityConfig.java
+++ backend/src/main/java/com/gac/configuration/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/api/user/authentification").permitAll().
+                .authorizeRequests().antMatchers("/api/user/authentification", "/api/user/create").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
--- backend/src/main/java/com/gac/service/JwtUserDetailsService.java
+++ backend/src/main/java/com/gac/service/JwtUserDetailsService.java
@@ -2,7 +2,11 @@ package com.gac.service;
 
 
 import java.util.ArrayList;
+import java.util.Optional;
 
+import com.gac.layer.dao.UserDao;
+import com.gac.modele.persistance.Users;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,12 +15,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserDao userDao;
 
-    // implements DAO
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("test".equals(username)) {
-            return new User("test", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+        Optional<Users> user = userDao.findByLogin(username);
+
+        if (user.isPresent()) {
+            return new User(user.get().getLogin(), user.get().getPassword(),
                     new ArrayList<>());
         } else {
             throw new UsernameNotFoundException("User not found with username: " + username);
